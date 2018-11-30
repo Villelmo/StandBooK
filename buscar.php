fa852f3530752c369e59a46300baaf4db72565d9
haywill20 <45340545+haywill20@users.noreply.github.com>
2018-11-30 11:34:28 -0600

    actualizacion

--- buscar.php
+++ buscar.php
@@ -197,23 +197,16 @@
 
 	<?php
 include 'conexion.php';
-$consulta = "SELECT * FROM libros where titulo='Matematica'";
+$consulta = "SELECT * FROM libros";
 $resultado = $con->query($consulta);
 //if($consulta->error) die($consulta->error);
 //Ejemplo para imprimir los datos. El bucle recorre todos los registros.
 while($fila = $resultado->fetch_assoc()) {
-
-    
-    echo "Titulo: " . $fila['titulo'];
+   
+    echo "Titulo: " . $fila['titulo'];  
 
 }
 ?>
-
-
-
-
-
-
 
 <!-- footer -->
   <footer id="footer">
