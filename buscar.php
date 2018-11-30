b6298aa92ea96001de809b068957472e2d0c356b
haywill20 <45340545+haywill20@users.noreply.github.com>
2018-11-30 08:36:44 -0600

    actualizacion

---
+++ buscar.php
@@ -0,0 +1,10 @@
+<?php
+include 'conexion.php';
+$consulta = "SELECT * FROM libros";
+$resultado = $con->query($consulta);
+//if($consulta->error) die($consulta->error);
+//Ejemplo para imprimir los datos. El bucle recorre todos los registros.
+while($fila = $resultado->fetch_assoc()) {
+    echo "Titulo: " . $fila['titulo'];
+}
+?>
